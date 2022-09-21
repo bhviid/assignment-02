3788e904030abd9d23e961f8ae67e7e421915ef2
Jacob Sonne <json@itu.dk>
2022-09-21 18:18:27 +0200

    Implemented Extension and LINQ Queries as well as tests for all of them

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -1,10 +1,171 @@
 namespace Assignment2.Tests;
+using static Assignment2.Queries;
 
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Extension_RowlingWizards()
     {
+        // Arrange
+        var expected = new List<String>{
+            "Harry Potter", 
+            "Harry Potter", 
+            "Harry Potter", 
+            "Hermione Granger", 
+            "Ronald Weasley", 
+            "Albus Dumbledore", 
+            "Voldemort", 
+            "Severus Snape"
+        };
+        
+        // Act
+        var actual = ExtensionRowlingWizards();
 
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Extension_FirstSith()
+    {
+        // Arrange
+        var expected = 1977;
+
+        // Act
+        var actual = ExtensionFirstSith();
+    
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Extension_UniqueHarryPotterWizards()
+    {
+        // Arrange
+        var expected = new List<(string, int?)>{
+            ("Harry Potter", 1997), 
+            ("Hermione Granger", 1997), 
+            ("Ronald Weasley", 1997), 
+            ("Albus Dumbledore", 1997), 
+            ("Voldemort", 1997), 
+            ("Severus Snape", 1997)
+        };                               
+    
+        // Act
+        var actual = ExtensionUniqueHarryPotterWizards();
+    
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Extension_SortedWizards()
+    {
+        // Arrange
+        var expected = new List<String>{
+            "Dr.Strange",
+            "Gandalf",
+            "Sauron",
+            "Sirius Black",
+            "Albus Dumbledore",
+            "Harry Potter",
+            "Harry Potter",
+            "Harry Potter",
+            "Hermione Granger",
+            "Ronald Weasley",
+            "Severus Snape",
+            "Voldemort",
+            "Darth Vader",
+            "Jafar"
+        };
+
+        // Act
+        var actual = ExtensionSortedWizards();
+    
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Linq_RowlingWizards()
+    {
+        // Arrange
+        var expected = new List<String>{
+            "Harry Potter", 
+            "Harry Potter", 
+            "Harry Potter", 
+            "Hermione Granger", 
+            "Ronald Weasley", 
+            "Albus Dumbledore", 
+            "Voldemort", 
+            "Severus Snape"
+        };
+
+        // Act
+        var actual = RowlingWizards();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Linq_FirstSith()
+    {
+        // Arrange
+        var expected = 1977;
+
+        // Act
+        var actual = FirstSith();
+    
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Linq_UniqueHarryPotterWizards()
+    {
+        // Arrange
+        var expected = new List<(string, int?)>{
+            ("Harry Potter", 1997), 
+            ("Hermione Granger", 1997), 
+            ("Ronald Weasley", 1997), 
+            ("Albus Dumbledore", 1997), 
+            ("Voldemort", 1997), 
+            ("Severus Snape", 1997)
+        };
+        
+        // Act
+        var actual = UniqueHarryPotterWizards();
+    
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Linq_SortedWizards()
+    {
+        // Arrange
+        var expected = new List<String>{
+            "Dr.Strange",
+            "Gandalf",
+            "Sauron",
+            "Sirius Black",
+            "Albus Dumbledore",
+            "Harry Potter",
+            "Harry Potter",
+            "Harry Potter",
+            "Hermione Granger",
+            "Ronald Weasley",
+            "Severus Snape",
+            "Voldemort",
+            "Darth Vader",
+            "Jafar"
+        };
+
+        // Act
+        var actual = SortedWizards();
+    
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,6 +1,90 @@
 namespace Assignment2;
-
+using System.Linq;
 public class Queries
 {
+    //Extenson Queries:
+    // Wizards invented by Rowling. Only return the names.
+    public static IEnumerable<String> ExtensionRowlingWizards(){
+        var wizards = WizardCollection.Create();
+
+        return wizards
+                .Where(w => w.Creator.Contains("Rowling"))
+                .Select(w => w.Name);
+    }
+
+    // The year the first sith lord was introduced. Let's define a sith lord to be one named Darth something.
+    public static int? ExtensionFirstSith(){
+        var wizards = WizardCollection.Create();
+
+        return wizards
+                .Where(w => w.Name.Contains("Darth"))
+                .Select(w => w.Year).FirstOrDefault();
+    }
+
+    // Unique list of wizards from the Harry Potter books - only return name and year (as a value tuple).
+    public static IEnumerable<(String, int?)> ExtensionUniqueHarryPotterWizards(){
+        var wizards = WizardCollection.Create();
+
+        return wizards
+                .Where(w => w.Creator.Contains("Rowling"))
+                .Select(w => (w.Name, w.Year))
+                .Distinct();
+    }
+
+    // List of wizard names grouped by creator in reverse order by creator name and then wizard name.
+    //Based on the description of the method I have interpreted that it is supposed to order by descending creator and then ascending name
+    public static IEnumerable<String> ExtensionSortedWizards(){
+        var wizards = WizardCollection.Create();
+        
+        return wizards
+                .OrderByDescending(w => w.Creator)
+                .ThenBy(w => w.Name)
+                .Select(w => w.Name);
+    }
+
+
+    //Linq Queries:
+    // Wizards invented by Rowling. Only return the names.
+    public static IEnumerable<String> RowlingWizards(){
+        var wizards = WizardCollection.Create();
+
+        return
+            from w in wizards
+            where w.Creator.Contains("Rowling")
+            select w.Name;
+
+    }
+
+    // The year the first sith lord was introduced. Let's define a sith lord to be one named Darth something.
+    //First som i den første på listen eller den med det laveste year?
+    public static int? FirstSith(){
+        var wizards = WizardCollection.Create();
+
+        return
+            (from w in wizards
+            where w.Name.Contains("Darth")
+            select w.Year).FirstOrDefault();
+    }
+
+    // Unique list of wizards from the Harry Potter books - only return name and year (as a value tuple).
+    public static IEnumerable<(string, int?)> UniqueHarryPotterWizards(){
+        var wizards = WizardCollection.Create();
+        
+        return
+            (from w in wizards
+            where w.Medium.Equals("Harry Potter")
+            select (w.Name, w.Year)).Distinct();
+    }
+
+    // List of wizard names grouped by creator in reverse order by creator name and then wizard name.
+    //Based on the description of the method I have interpreted that it is supposed to order by descending creator and then ascending name
+    public static IEnumerable<String> SortedWizards(){
+        var wizards = WizardCollection.Create();
+
+        return
+            from w in wizards
+            orderby w.Creator descending, w.Name
+            select w.Name;
+    }
 
 }
