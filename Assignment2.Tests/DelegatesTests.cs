6a7dca042781d17df5259175f1b9880793c49299
Bertram <behv@itu.dk>
2022-09-22 19:23:40 +0200

    Delegates tests

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -3,26 +3,49 @@ namespace Assignment2.Tests;
 public class DelegatesTests
 {
     [Fact]
-    public void Test1()
+    public void ReverseString_Should_Print_hello()
     {
-      //  Func<string> reverseString = s => Console.WriteLine(s.Reverse());
+        Action<string> ReverseString = s => 
+        {
+            foreach (var n in s.Reverse().ToList())
+            {
+                Console.Write(n);
+            }
+        };
         
-        
-    }
-    [Fact]
-    public void Test2()
-    {
+        var sw = new StringWriter();
+        Console.SetOut(sw);
+       
+        ReverseString("olleh");
    
-        Func<Decimal,Decimal,Decimal> Product = (d1, d2) => d1 * d2; 
-        
-        
+        Assert.Equal("hello",sw.ToString());
         
         
     }
     [Fact]
-    public void Test3()
+    public void Product_Returns_6_When_Given_2_3()
+    {
+        Func<Decimal,Decimal,Decimal> Product = (d1, d2) => d1 * d2;
+
+        var d1 = 2;
+        var d2 = 3;
+        
+        Assert.Equal(6,Product(d1,d2));
+
+
+
+    }
+    [Fact]
+    public void IsEqual_Returns_True_Given_TwoNummericallyEqualInputs()
     {
         Func<int,string,bool> IsEqual = (i, s) => int.TryParse(s, out var x) ? i == x : throw new Exception("Invalid Input"); 
+        
+        int i = 22;
+        string s = "22";
+        
+        Assert.True(IsEqual(i,s));
+        
+        
     }
     
 }
